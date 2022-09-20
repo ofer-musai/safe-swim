6b37bccb61abfebfe2d7ad4a8a66ac74719c97a6
ofermusai <ofermusai@microsoft.com>
2022-09-20 15:12:40 +0300

    init

---
+++ my-app/src/App.js
@@ -0,0 +1,58 @@
+import React, {useEffect, useState} from 'react';
+import './App.css';
+import alarmSrc from './alarm.wav';
+
+let intervalId = null;
+const INTERVAL_DELAY = 1000;
+
+const sound = new Audio(alarmSrc);
+sound.loop = true;
+
+const getData = async () => {
+    const res = await fetch('http://192.168.4.1/status');
+    const data = await res.json();
+    console.log('data', data);
+    return data;
+}
+
+function App() {
+    const [isActive, setIsActive] = useState(false);
+    useEffect(() => {
+        const polling = async () => {
+            const data = await getData();
+            setIsActive(data === 1);
+        };
+
+        polling();
+        intervalId = setInterval(polling, INTERVAL_DELAY);
+
+        return () => {
+            clearInterval(intervalId)
+        }
+    }, []);
+
+    useEffect(() => {
+        if(isActive) {
+            // sound.currentTime = 0;
+            // sound.play();
+        }else {
+            // sound.pause();
+        }
+    }, [isActive]);
+
+
+    const muteSound = () => {
+        sound.pause();
+    };
+
+    return (
+        <div className="App">
+            <div className="header">Safe Swim</div>
+            <div className="content">
+                <div onClick={muteSound} className={`status ${isActive ? 'active' : 'in-active' }`}></div>
+            </div>
+        </div>
+  );
+}
+
+export default App;
