58b6216fba5589f306cfe9f688f0af1945729524
guymeiri <guymeiri@microsoft.com>
2022-09-21 16:17:42 +0300

    added mock server login with an invisible button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
-import React, {useEffect, useState} from 'react';
-import './App.css';
-import alarmSrc from './alarm.wav';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import alarmSrc from "./alarm.wav";
 
 let intervalId = null;
 const INTERVAL_DELAY = 1000;
@@ -9,191 +9,226 @@ const RETRY_MAX = 3;
 const sound = new Audio(alarmSrc);
 sound.loop = true;
 
-
 async function fetchWithTimeout(resource, options = {}) {
-    const { timeout = 2000 } = options;
+  const { timeout = 2000 } = options;
 
-    const controller = new AbortController();
-    const id = setTimeout(() => controller.abort(), timeout);
-    const response = await fetch(resource, {
-        ...options,
-        signal: controller.signal
-    });
-    clearTimeout(id);
-    return response;
+  const controller = new AbortController();
+  const id = setTimeout(() => controller.abort(), timeout);
+  const response = await fetch(resource, {
+    ...options,
+    signal: controller.signal,
+  });
+  clearTimeout(id);
+  return response;
 }
 
 const getData = async () => {
-    const res = await fetchWithTimeout('http://192.168.4.1/status');
-    const data = await res.json();
-    console.log('data', data);
-    return data;
-}
-
-
-
-const ConnectionStep = ({onNext}) => {
-    const [error, setError] = useState(false);
-
-    // useEffect(() => {
-    //     setTimeout(() => {
-    //         onNext();
-    //     },3000)
-    // })
-
-    const connect = () => {
-        let retryCount = 0;
-
-        const polling = async () => {
-            try {
-                const data = await getData();
-                console.log('data', data);
-                onNext();
-            }catch (e) {
-                console.log('e', e);
-                if(retryCount < RETRY_MAX){
-                    retryCount++;
-                    setTimeout(() => {
-                        polling();
-                    }, 1000)
-                }else {
-                    setError(true)
-                }
-            }
-        };
-        polling();
-    }
-
-
-    useEffect(() => {
-        connect();
-    }, []);
-
-    const reconnect = () => {
-        connect();
-        setError(false);
-     }
-
-
-    if(error){
-        return  (
-            <div className="step-container">
-                <div className="description">Cant connect to device.</div>
-                <div className="btn" onClick={reconnect}>Reconnect</div>
-            </div>
-        )
-    }
-
-
-    return  (
-        <div className="step-container">
-            <div className="description">First let's see if the connection its working.</div>
-            <div className="loader"/>
-            <div></div>
-        </div>
-    )
+  const res = await fetchWithTimeout("http://192.168.4.1/status");
+  const data = await res.json();
+  console.log("data", data);
+  return data;
 };
 
+const ConnectionStep = ({ onNext, isMockServer }) => {
+  const [error, setError] = useState(false);
 
-const TestingStep = ({onNext}) => {
-    const [isTested, setIsTested] = useState(false);
-    useEffect(() => {
-        const polling = async () => {
-            const data = await getData();
+  const connect = (isMockServer) => {
+    let retryCount = 0;
 
-            if(!isTested && data === 1){
-                setIsTested(true);
-            }
-        };
+    const polling = async () => {
+      if (isMockServer) {
+        setTimeout(() => {
+          onNext();
+        }, 1000);
+        return;
+      }
 
-        polling();
-        intervalId = setInterval(polling, INTERVAL_DELAY);
-
-        return () => {
-            clearInterval(intervalId)
+      try {
+        const data = await getData();
+        console.log("data", data);
+        onNext();
+      } catch (e) {
+        console.log("e", e);
+        if (retryCount < RETRY_MAX) {
+          retryCount++;
+          setTimeout(() => {
+            polling();
+          }, 1000);
+        } else {
+          setError(true);
         }
-    }, []);
+      }
+    };
+    polling();
+  };
 
+  useEffect(() => {
+    connect(isMockServer);
+  }, []);
 
-    return  (
-        <div className="step-container">
-            <div className="description">Make sure to test it before using it</div>
-            <div className={`icon testing-icon ${isTested ? 'enable' : 'disable'}`}/>
-            <div className={`btn ${isTested ? 'enable' : 'disable'}`} onClick={isTested ? onNext : () => {}}>Done</div>
+  const reconnect = () => {
+    connect();
+    setError(false);
+  };
+
+  if (error) {
+    return (
+      <div className="step-container">
+        <div className="description">Cant connect to device.</div>
+        <div className="btn" onClick={reconnect}>
+          Reconnect
         </div>
-    )
+      </div>
+    );
+  }
+
+  return (
+    <div className="step-container">
+      <div className="description">
+        First let's see if the connection its working.
+      </div>
+      <div className="loader" />
+      <div></div>
+    </div>
+  );
 };
 
-const Main = () => {
-    const [isActive, setIsActive] = useState(false);
-    useEffect(() => {
-        const polling = async () => {
-            const data = await getData();
-            setIsActive(data === 1);
-        };
+const TestingStep = ({ onNext, isMockServer }) => {
+  const [isTested, setIsTested] = useState(false);
 
-        polling();
-        intervalId = setInterval(polling, INTERVAL_DELAY);
+  useEffect(() => {
+    const polling = async () => {
+      if (isMockServer) {
+        setTimeout(() => {
+          setIsTested(true);
+        }, INTERVAL_DELAY * 2);
 
-        return () => {
-            clearInterval(intervalId)
-        }
-    }, []);
+        return;
+      }
 
-    useEffect(() => {
-        if(isActive) {
-            sound.currentTime = 0;
-            sound.play();
-        }else {
-            sound.pause();
-        }
-    }, [isActive]);
+      const data = await getData();
 
-
-    const muteSound = () => {
-        sound.pause();
+      if (!isTested && data === 1) {
+        setIsTested(true);
+      }
     };
 
-    return (
-            <>
-                <div className="image"/>
-                <div onClick={muteSound} className={`status ${isActive ? 'active' : 'in-active' }`}>
-                    <div className="ocean">
-                        <div className="wave"></div>
-                        <div className="wave"></div>
-                    </div>
+    polling();
+    intervalId = setInterval(polling, INTERVAL_DELAY);
 
-                </div>
-            </>
-    );
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return (
+    <div className="step-container">
+      <div className="description">Make sure to test it before using it</div>
+      <div className={`icon testing-icon ${isTested ? "enable" : "disable"}`} />
+      <div
+        className={`btn ${isTested ? "enable" : "disable"}`}
+        onClick={isTested ? onNext : () => {}}
+      >
+        Done
+      </div>
+    </div>
+  );
 };
 
+let isMockReturnActiveCurrIndex = 1;
+const MOCK_RETURNS_ACTIVE_INDEX = 14;
+
+const Main = ({ isMockServer }) => {
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    const polling = async () => {
+      const data = await getData();
+      setIsActive(data === 1);
+    };
+
+    const mockPolling = () => {
+      setIsActive(isMockReturnActiveCurrIndex % MOCK_RETURNS_ACTIVE_INDEX > 7);
+      isMockReturnActiveCurrIndex =
+        (isMockReturnActiveCurrIndex + 1) % MOCK_RETURNS_ACTIVE_INDEX;
+    };
+
+    intervalId = setInterval(
+      isMockServer ? mockPolling : polling,
+      INTERVAL_DELAY
+    );
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isActive) {
+      sound.currentTime = 0;
+      sound.play();
+    } else {
+      sound.pause();
+    }
+  }, [isActive]);
+
+  const muteSound = () => {
+    sound.pause();
+  };
+
+  return (
+    <>
+      <div className="image" />
+      <div
+        onClick={muteSound}
+        className={`status ${isActive ? "active" : "in-active"}`}
+      >
+        <div className="ocean">
+          <div className="wave"></div>
+          <div className="wave"></div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 function App() {
-    const [step, setStep] = useState(1);
+  const [step, setStep] = useState(1);
+  const [isMockServer, setIsMockServer] = useState(false);
 
-    const onNext = () => {
-        setStep(step + 1)
+  const mockServerButtonHandler = () => {
+    const newIsMockServer = !isMockServer;
+    if (newIsMockServer) {
+      setStep(2);
     }
 
-    const Comp = step === 3 ? Main : step === 2 ? TestingStep : ConnectionStep;
+    setIsMockServer(newIsMockServer);
+  };
 
-    return (
-        <div className="App">
-            <div className="header">
-                <div className="header-logo"/>
-                <div className="header-text">Safe Swim</div>
-            </div>
-            <div className="content">
-                <div className="image"/>
-                <Comp onNext={onNext}/>
-            </div>
+  const onNext = () => {
+    setStep(step + 1);
+  };
 
-            <div className="wave pre-loading"></div>
+  const Comp = step === 3 ? Main : step === 2 ? TestingStep : ConnectionStep;
 
-        </div>
-    )
+  return (
+    <div className="App">
+      <button className="mock-button" onClick={mockServerButtonHandler}>
+        Mock Server
+      </button>
+      {isMockServer && <div className="demo-text">Demo</div>}
+      <div className="header">
+        <div className="header-logo" />
+        <div className="header-text">Safe Swim</div>
+      </div>
+      <div className="content">
+        <div className="image" />
+        <Comp onNext={onNext} isMockServer={isMockServer} />
+      </div>
 
+      <div className="wave pre-loading"></div>
+    </div>
+  );
 }
 
 export default App;
